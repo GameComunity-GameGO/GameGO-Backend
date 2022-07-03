6616e31c65f501d186975bb02b2e6a73042919f0
Lee.t.c <seonghoo1217@naver.com>
2022-07-03 22:42:11 +0900

    #1 feature: Swagger Config and Web Config for the CORS

---
+++ gamego/src/main/java/sideproject/junior/gamego/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package sideproject.junior.gamego.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    private static final String API_NAME = "Game GO API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "Game GO API Request Spec";
+
+
+    @Bean
+    public Docket swaggerAPI(){
+        //Docket : swagger Bean
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true) //기본 응답 메시지 표시 여부
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("sideproject.junior.gamego")) //swagger탐색 대상 패키지
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .build();
+    }
+
+}

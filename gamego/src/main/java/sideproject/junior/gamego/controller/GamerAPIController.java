91cdca8f1128757e5e08db330c75ac5f6af0537d
Lee.t.c <seonghoo1217@naver.com>
2022-08-13 23:56:25 +0900

    GamerAPI featuring

---
+++ gamego/src/main/java/sideproject/junior/gamego/controller/GamerAPIController.java
@@ -0,0 +1,39 @@
+package sideproject.junior.gamego.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sideproject.junior.gamego.model.dto.GamerDTO;
+import sideproject.junior.gamego.model.entity.Gamer;
+import sideproject.junior.gamego.service.GamerService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/v1")
+public class GamerAPIController {
+
+    private final GamerService gamerService;
+
+    @GetMapping("/GamerList")
+    public ResponseEntity<List<Gamer>> getGamerListApi(){
+        List<Gamer> gamerList = gamerService.getGamerListApi();
+        return new ResponseEntity<>(gamerList, HttpStatus.OK);
+    }
+
+    @GetMapping("/gamer/{id}")
+    public ResponseEntity<Gamer> getGamerState(@PathVariable Long id){
+        Gamer gamer = gamerService.getGamerApi(id);
+        return new ResponseEntity<>(gamer,HttpStatus.OK);
+    }
+
+    @PostMapping("/gamer")
+    public ResponseEntity<?> gamerRegistation(GamerDTO.GamerRegistationDTO gamerRegistationDTO){
+        gamerService.registationGamerApi(gamerRegistationDTO);
+        return new ResponseEntity<>("게이머등록 API 성공",HttpStatus.OK);
+    }
+}
---
+++ gamego/src/main/java/sideproject/junior/gamego/service/GamerService.java
@@ -0,0 +1,48 @@
+package sideproject.junior.gamego.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sideproject.junior.gamego.model.dto.GamerDTO;
+import sideproject.junior.gamego.model.entity.Gamer;
+import sideproject.junior.gamego.model.entity.Member;
+import sideproject.junior.gamego.principal.SecurityUtil;
+import sideproject.junior.gamego.repository.GamerRepository;
+import sideproject.junior.gamego.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GamerService {
+
+    private final GamerRepository gamerRepository;
+
+    private final SecurityUtil securityUtil;
+
+    private final MemberService memberService;
+
+    public List<Gamer> getGamerListApi(){
+        List<Gamer> all = gamerRepository.findAll();
+        return all;
+    }
+
+    public Gamer getGamerApi(Long id){
+        Gamer gamer = gamerRepository.findById(id).get();
+        return gamer;
+    }
+
+    @Transactional
+    public void registationGamerApi(GamerDTO.GamerRegistationDTO gamerRegistationDTO){
+        String username = securityUtil.returnLoginMemberInfo();
+        Member findMember = memberService.MemberStateApi(username);
+        gamerRegistationDTO.setMember(findMember);
+        Gamer entity = gamerRegistationDTO.toEntity();
+        gamerRepository.save(entity);
+
+    }
+
+}

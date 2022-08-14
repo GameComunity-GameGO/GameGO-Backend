9bd68029ac638edc6d74dfd2f0b4117018050f1d
Lee.t.c <seonghoo1217@naver.com>
2022-08-14 21:48:51 +0900

    #1 fix: Security Swagger URL, Jwt AccessToken Expiration

--- gamego/src/main/java/sideproject/junior/gamego/config/security/SecurityConfig.java
+++ gamego/src/main/java/sideproject/junior/gamego/config/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web){
         web.ignoring().antMatchers("/swagger-ui/**","/webjars/**","/css/**","/js/**","/v2/api-docs","/v3/api-docs" ,"/configuration/ui",
-                "/swagger-resources", "/configuration/security",
+                "/swagger-resources/**", "/configuration/security",
                 "/swagger-ui.html", "/webjars/**","/swagger/**");
     }
 
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/SignUp/**","/api/v1/member","/api/v1/login","/api/v1/accessToken").permitAll()
+                .antMatchers("/api/v1/SignUp/**","/api/v1/member","/api/v1/login","/api/v1/accessToken","/v3/api-docs","/swagger/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated();
 

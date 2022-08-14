45bed948e31053c7cbed512218527feccbec2df4
Lee.t.c <seonghoo1217@naver.com>
2022-08-15 01:42:16 +0900

    #1 fix :JSON IGNORE to MEMber ENtity

--- gamego/src/main/java/sideproject/junior/gamego/controller/MemberAPIController.java
+++ gamego/src/main/java/sideproject/junior/gamego/controller/MemberAPIController.java
@@ -21,7 +21,9 @@ public class MemberAPIController {
 
     @GetMapping("/members")
     public ResponseEntity<List<Member>> memberList(){
+        log.info("멤버리스트");
         List<Member> members = memberService.MemberListApi();
+        System.out.println("members = " + members);
         return new ResponseEntity<>(members,HttpStatus.OK);
     }
 
@@ -41,7 +43,7 @@ public class MemberAPIController {
     }
 
     @DeleteMapping("/member")
-    public ResponseEntity<String> deleteMember(MemberDTO.DeleteDTO deleteDTO){
+    public ResponseEntity<String> deleteMember(@RequestBody MemberDTO.DeleteDTO deleteDTO){
         String deleteApiState = memberService.MemberDeleteApi(deleteDTO);
         return new ResponseEntity<>(deleteApiState,HttpStatus.OK);
     }

06258213c552ecc7d84dd35b761f639a1a7bd970
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-08-15 00:25:56 +0900

    Update BaseEntity.java

--- gamego/src/main/java/sideproject/junior/gamego/model/entity/BaseEntity.java
+++ gamego/src/main/java/sideproject/junior/gamego/model/entity/BaseEntity.java
@@ -2,13 +2,16 @@ package sideproject.junior.gamego.model.entity;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import lombok.Getter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.EntityListeners;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@Getter
 public abstract class BaseEntity {
 
     @CreatedDate

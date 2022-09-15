8990b55229b18209ddc6afa6432dc0722e14785a
Seong su Ha <hss25350@naver.com>
2022-09-15 19:29:56 +0900

    feature/image_api_fix

--- gamego/src/main/java/sideproject/junior/gamego/repository/images/ImagesRepository.java
+++ gamego/src/main/java/sideproject/junior/gamego/repository/images/ImagesRepository.java
@@ -5,7 +5,7 @@ import sideproject.junior.gamego.model.entity.Images;
 
 import java.util.List;
 
-public interface ImagesRepository extends JpaRepository<Images, Long>, ImageCustomRepository{
+public interface ImagesRepository extends JpaRepository<Images, Long>{
 
     List<Images> findAllByCommunityBoardId(Long communityBoardId);
 }

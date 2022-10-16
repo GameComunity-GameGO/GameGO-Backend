be0101e2237c50eda4b1e0b5c15674f25cd80c64
Seong su Ha <hss25350@naver.com>
2022-10-16 21:14:46 +0900

    feature/chat_api_develop

---
+++ gamego/src/main/java/sideproject/junior/gamego/handler/StompHandler.java
@@ -0,0 +1,47 @@
+package sideproject.junior.gamego.handler;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class StompHandler implements ChannelInterceptor {
+
+    @Value("spring.jwt.secret")
+    private String secretKey;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        System.out.println("message:" + message);
+        System.out.println("헤더 : " + message.getHeaders());
+        System.out.println("토큰" + accessor.getNativeHeader("Authorization"));
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            validateToken(Objects.requireNonNull(accessor.getFirstNativeHeader("Authorization")).substring(7));
+        }
+        return message;
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}

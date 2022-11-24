2bd6d3b54b83dfc3fca0f251801c78e395b23067
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-11-24 18:18:51 +0900

    Update WebSocketConfig.java

--- gamego/src/main/java/sideproject/junior/gamego/config/WebSocketConfig.java
+++ gamego/src/main/java/sideproject/junior/gamego/config/WebSocketConfig.java
@@ -1,37 +1,37 @@
-package sideproject.junior.gamego.config;
+// package sideproject.junior.gamego.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import sideproject.junior.gamego.handler.StompHandler;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.ChannelRegistration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import sideproject.junior.gamego.handler.StompHandler;
 
-@Configuration
-@EnableWebSocketMessageBroker
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+// @Configuration
+// @EnableWebSocketMessageBroker
+// @RequiredArgsConstructor
+// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompHandler stompHandler;
+//     private final StompHandler stompHandler;
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/alarm", "/ws/chat")
-                /*.setAllowedOrigins("http://localhost:3000")*/
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         registry.addEndpoint("/ws/alarm", "/ws/chat")
+//                 /*.setAllowedOrigins("http://localhost:3000")*/
+//                 .setAllowedOriginPatterns("*")
+//                 .withSockJS();
+//     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/ws/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry registry) {
+//         registry.enableSimpleBroker("/ws/topic");
+//         registry.setApplicationDestinationPrefixes("/app");
+//     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompHandler);
-    }
-}
+//     @Override
+//     public void configureClientInboundChannel(ChannelRegistration registration) {
+//         registration.interceptors(stompHandler);
+//     }
+// }

fba1122e90dc64db212f64274999851c522d907e
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-12-08 20:42:42 +0900

    Update ChatMessageCustomRepositoryImpl.java

--- gamego/src/main/java/sideproject/junior/gamego/repository/chat/ChatMessageCustomRepositoryImpl.java
+++ gamego/src/main/java/sideproject/junior/gamego/repository/chat/ChatMessageCustomRepositoryImpl.java
@@ -3,12 +3,13 @@ package sideproject.junior.gamego.repository.chat;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import sideproject.junior.gamego.model.entity.QChatMessage;
 import sideproject.junior.gamego.model.entity.QChatRoom;
-
+import lombok.extern.log4j.Log4j2;
 import javax.persistence.EntityManager;
 
 import static sideproject.junior.gamego.model.entity.QChatMessage.*;
 import static sideproject.junior.gamego.model.entity.QChatRoom.*;
 
+@Log4j2
 public class ChatMessageCustomRepositoryImpl implements ChatMessageCustomRepository{
 
     private final JPAQueryFactory queryFactory;
@@ -20,10 +21,14 @@ public class ChatMessageCustomRepositoryImpl implements ChatMessageCustomReposit
     @Override
     public Long getNoticeCount(Long roomId, Long messageId) {
 
-        int size = queryFactory.selectFrom(chatMessage)
+        log.info("====================== ChatMessageRepository.getNoticeCount ========================");
+        
+        Integer size = queryFactory.selectFrom(chatMessage)
                 .where(chatMessage.chatRoom.id.eq(roomId))
                 .where(chatMessage.id.gt(messageId))
                 .fetch().size();
+
+        log.info("-------------- size = " + size + "---------------");
 
         return (long) size;
     }

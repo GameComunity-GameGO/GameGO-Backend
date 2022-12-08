8aa50539c88a6e5184073e945cc8743318b882d8
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-12-08 20:04:02 +0900

    Update NoticeService.java

--- gamego/src/main/java/sideproject/junior/gamego/service/NoticeService.java
+++ gamego/src/main/java/sideproject/junior/gamego/service/NoticeService.java
@@ -8,6 +8,7 @@ import sideproject.junior.gamego.model.dto.chat.ResChatRoomDTO;
 import sideproject.junior.gamego.model.entity.ChatRoomJoinMember;
 import sideproject.junior.gamego.model.entity.Member;
 import sideproject.junior.gamego.repository.chat.ChatRoomRepository;
+import sideproject.junior.gamego.repository.chat.ChatMessageRepository;
 import sideproject.junior.gamego.repository.MemberRepository;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class NoticeService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final MemberRepository memberRepository;
+    private final ChatMessageRepository chatMessageRepository;
 
 
     public List<ResChatRoomDTO> getChatNoticeList(Long memberId) {
@@ -30,7 +32,7 @@ public class NoticeService {
         List<ResChatRoomDTO> getRoomList = new ArrayList<>();
 
         for (ChatRoomJoinMember chatRoomJoinMember : member.getChatRoomJoinMembers()) {
-            Long noticeCount = chatRoomRepository.getNoticeCount(chatRoomJoinMember.getChatRoom().getId(), chatRoomJoinMember.getCheckPoint());
+            Long noticeCount = chatMessageRepository.getNoticeCount(chatRoomJoinMember.getChatRoom().getId(), chatRoomJoinMember.getCheckPoint());
             ResChatRoomDTO resChatRoomDTO = chatRoomJoinMember.getChatRoom().toResDTO();
             resChatRoomDTO.setNoticeCount(noticeCount);
             getRoomList.add(resChatRoomDTO);

9d8004a4fcd1a4a4d15a7821e76968b8597f7d17
Seong su Ha <73016759+hss512@users.noreply.github.com>
2022-12-14 23:50:36 +0900

    Update AwsS3Config.java

--- gamego/src/main/java/sideproject/junior/gamego/config/AwsS3Config.java
+++ gamego/src/main/java/sideproject/junior/gamego/config/AwsS3Config.java
@@ -1,33 +1,33 @@
-package sideproject.junior.gamego.config;
+// package sideproject.junior.gamego.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.amazonaws.auth.AWSStaticCredentialsProvider;
+// import com.amazonaws.auth.BasicAWSCredentials;
+// import com.amazonaws.services.s3.AmazonS3Client;
+// import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class AwsS3Config {
+// @Configuration
+// public class AwsS3Config {
 
-   @Value("${ackey}")
-   private String accessKey;
+//    @Value("${ackey}")
+//    private String accessKey;
 
-   @Value("${sckey}")
-   private String secretKey;
+//    @Value("${sckey}")
+//    private String secretKey;
 
-   @Value("${cloud.aws.region.static}")
-   private String region;
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
 
-   @Bean
-   public AmazonS3Client amazonS3Client() {
+//    @Bean
+//    public AmazonS3Client amazonS3Client() {
 
-       BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+//        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
 
-       return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-               .withRegion(region)
-               .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-               .build();
-   }
-}
+//        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+//                .withRegion(region)
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+//                .build();
+//    }
+// }
